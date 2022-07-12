5be755f3318fc046ea7f47e92a53f8ad2ff8b665
CortUA <104569441+CortUA@users.noreply.github.com>
2022-07-12 16:11:24 +0300

    12.07.2022

---
+++ src/test/java/PageObjects/UkrPartsPageHelper.java
@@ -0,0 +1,15 @@
+package PageObjects;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class UkrPartsPageHelper extends UkrPartsPageElements{
+    public UkrPartsPageHelper(WebDriver driver){
+        super(driver);
+    }
+
+    }
+
+
+
+
---
+++ src/test/java/TestNG/TestPartsSearch.java
@@ -0,0 +1,18 @@
+package TestNG;
+
+import PageObjects.UkrPartsPageHelper;
+import holders.TestInit;
+import org.testng.annotations.Test;
+
+public class TestPartsSearch extends TestInit {
+
+
+    @Test
+    public void ukrParts(){
+        UkrPartsPageHelper ukrPartsPageHelper = new UkrPartsPageHelper(driver);
+        ukrPartsPageHelper.goToHomePage();
+
+
+
+    }
+}
